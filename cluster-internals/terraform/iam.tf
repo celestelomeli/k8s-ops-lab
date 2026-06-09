3945db7b5848f9879a17cd0323a6d03ab65b220c
celestelomeli <celesteelomeli@gmail.com>
2026-06-09 15:46:42 -0700

    add ansible automation to provision and join kubernetes cluster nodes

--- cluster-internals/terraform/iam.tf
+++ cluster-internals/terraform/iam.tf
@@ -20,3 +20,20 @@ resource "aws_iam_instance_profile" "ssm" {
   name = "ec2-ssm-role"
   role = aws_iam_role.ssm.name
 }
+
+resource "aws_iam_role_policy" "ansible_s3" {
+  name = "ansible-ssm-s3"
+  role = aws_iam_role.ssm.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"]
+      Resource = [
+        "arn:aws:s3:::k8s-ops-lab-tfstate",
+        "arn:aws:s3:::k8s-ops-lab-tfstate/*"
+      ]
+    }]
+  })
+}
